5febd4f86166d8ef8369cd9f0c4d786ce685dbea
hlz <qz338338@163.com>
2018-08-03 19:21:52 +0800

    模板输出assign 变量

--- LFrame/Controller.php
+++ LFrame/Controller.php
@@ -2,6 +2,8 @@
 namespace LFrame;
 class Controller{
 
+    public $view_vars = [];
+  
     public function init($action){
         $this->{$action}();
         $this->display();
@@ -9,16 +11,21 @@ class Controller{
 
     function __destruct() {
         //$this->display();
-   }
+    }
    
+    function assign($key, $val){
+      $this->view_vars[$key] = $val;
+    }
+
    //输出模板
    protected function display($path = ""){
-        
+        extract($this->view_vars);
         require_once(
             empty($path) ? 
             sprintf("%s/%s/%s%s", VIEW_PATH, __CONTROLLER__, __ACTION__, ".html")  :
             sprintf("%s/%s%s", VIEW_PATH, $path, ".html")
         );
+        exit();
    }
 }
 
